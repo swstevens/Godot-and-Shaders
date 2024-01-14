320c1db65214a51ad8e251754ff5a3077604b825
swstevens <sstevensw@gmail.com>
2024-01-14 14:49:40 -0800

    create scene to use with shaders

---
+++ Camera3D.gd
@@ -0,0 +1,23 @@
+extends Camera3D
+
+var d := 0.0
+var radius := 10
+var speed := 1.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	d += delta
+	position = Vector3(
+		sin(d * speed) * radius,
+		0.0,
+		cos(d * speed) * radius,
+	) + Vector3(0,2,0)
+	rotation = Vector3(
+		0.0,
+		d,
+		0.0
+	) + Vector3(-.08,0,0)
